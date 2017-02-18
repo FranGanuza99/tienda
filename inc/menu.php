106b199dc364d9f3bd9ae7b9dfadce3cd6e4c823
Francisco Ganuza <Francisco Ganuza>
2017-02-17 23:13:52 -0600

    Initial Commit

---
+++ inc/menu.php
@@ -0,0 +1,22 @@
+<div class="navbar-fixed">
+    <nav>
+    <div class="nav-wrapper green" >
+        <div class="container">
+        <a href="#" class="brand-logo"><img class="logo" src="img/logo/logo.png"></a>
+        <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">item1</a></li>
+            <li><a href="#">item2</a></li>
+            <li><a href="#">item3</a></li>
+            <li><a href="#">item4</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-menu">
+            <li><a href="#">item1</a></li>
+            <li><a href="#">item2</a></li>
+            <li><a href="#">item3</a></li>
+            <li><a href="#">item4</a></li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <!--Import Google Icon Font-->
+        <link href="css/icons.css" rel="stylesheet">
+        <title>Ricaldone | ITR </title>
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/icon.css"  media="screen,projection"/>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!--  archivos css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style-sheet.css" media="screen,projection"/>
+    </head>
+    <body>
+        <!--Aqui se muestra el menu-->
+        <?php
+        include("inc/menu.php");
+        ?>
+
+        <!--Aqui comienza la pagina-->
+        <div class="container">
+            
+
+
+        </div>
+        
+        <!--Aqui se muestra el pie de pagina-->
+        <?php
+        include("inc/footer.php");
+        ?>
+        
+
+    </body>
+</html>
