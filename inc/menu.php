f8edfe77af87f526e175d14e0e886d4bcc80e643
Francisco Ganuza <Francisco Ganuza>
2017-02-19 20:19:44 -0600

    Modificaciones de algunos errores

--- inc/menu.php
+++ inc/menu.php
@@ -1,22 +1,27 @@
 <div class="navbar-fixed">
     <nav>
-    <div class="nav-wrapper green" >
+    <div class="nav-wrapper blue" >
         <div class="container">
         <a href="#" class="brand-logo"><img class="logo" src="img/logo/logo.png"></a>
         <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#">item1</a></li>
-            <li><a href="#">item2</a></li>
-            <li><a href="#">item3</a></li>
-            <li><a href="#">item4</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Catálogo de autos</a></li>
+            <li><a href="#">Nuestra empresa</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Guia para comprar</a></li>
+            <li class="active"><a href="#">Iniciar sesión</a></li>
         </ul>
-        <ul class="side-nav" id="mobile-menu">
-            <li><a href="#">item1</a></li>
-            <li><a href="#">item2</a></li>
-            <li><a href="#">item3</a></li>
-            <li><a href="#">item4</a></li>
-        </ul>
+        
         </div>
     </div>
     </nav>
+    
 </div>
+
+<ul class="side-nav" id="mobile-menu">
+    <li><a href="#">item1</a></li>
+    <li><a href="#">item2</a></li>
+    <li><a href="#">item3</a></li>
+    <li><a href="#">item4</a></li>
+</ul>
